d63394864f693f2262f341874a30fd661c46a4ec
fsolowjow <friedrich.solowjow2@gmail.com>
2016-01-21 23:32:16 +0100

    Some bugs fixed

--- jacobi.py
+++ jacobi.py
@@ -10,17 +10,26 @@ from matplotlib import animation
 from scipy.integrate import quad
 from scipy.optimize import brentq
 
-def coeff_a(n , alpha , beta):
-	return (2/(2*n + alpha + beta ) * np.sqrt((n*(n+alpha+beta)*(n+alpha)*(n+beta)) / ((2*n+alpha+beta-1)*(2*n+alpha+beta+1))))
+#wikipedia formula for a and b in T matrix, since def in lecture does not work:
+def coeff_a_matrix(n , alpha , beta):
+	if(n==0):	return (beta-alpha)/(2+alpha+beta)
+	else:		return (beta-alpha)*(alpha+beta)/((2*n+alpha+beta)*(2*n+2+alpha+beta))
 
-def coeff_b(n , alpha , beta):
+def coeff_b_matrix(n , alpha , beta):
+	return np.sqrt((4*n*(n+alpha)*(n+beta)*(n+alpha+beta))/((2*n-1+alpha+beta)*(2*n+alpha+beta)*(2*n+alpha+beta)*(2*n+1+alpha+beta)))
+
+#Lecture formula for recursion coeffs.
+def coeff_a_rec(n , alpha , beta):
+	return (2/(2*n + alpha + beta  ) * np.sqrt((n*(n+alpha+beta)*(n+alpha)*(n+beta)) / ((2*n+alpha+beta-1)*(2*n+alpha+beta+1))))
+	
+def coeff_b_rec(n , alpha , beta):
 	return -(alpha*alpha - beta*beta)/((2*n+alpha+beta)*(2*n+alpha+beta+2))
 
 #a)	
 def jacobiP(x, alpha, beta, n):
 	if(n==0):	return np.full ( np.size(x) , np.sqrt( np.power(2., -alpha - beta - 1) * scipy.special.gamma(alpha+beta+2) / (scipy.special.gamma(alpha+1)* scipy.special.gamma(beta+1))))
 	if(n==1):	return 0.5 * np.sqrt((alpha+beta+3)/((alpha+1)*(beta+1))) *((alpha+beta+2)*x + alpha - beta) * jacobiP(x,alpha,beta,0)
-	if(n>1):	return ( (x*jacobiP(x,alpha,beta, n-1) - coeff_a(n-1,alpha,beta) * jacobiP(x,alpha,beta, n-2) - coeff_b(n-1,alpha,beta) * jacobiP(x,alpha,beta, n-1)) / coeff_a(n,alpha,beta))
+	if(n>1):	return ( (x*jacobiP(x,alpha,beta, n-1) - coeff_a_rec(n-1,alpha,beta) * jacobiP(x,alpha,beta, n-2) - coeff_b_rec(n-1,alpha,beta) * jacobiP(x,alpha,beta, n-1)) / coeff_a_rec(n,alpha,beta))
 
 #b)	
 def jacobiDer(x, alpha, beta, n):
@@ -33,20 +42,20 @@ def integrand(x):
 def JacobiQuad( n, alpha , beta):
 	T=np.zeros((n+1,n+1))
 	for i in range(n+1):
-		T[i,i] = coeff_b(i+1 , alpha , beta)	#Since b_0 does not make sense
-		if(i < n): T[i,i+1] = coeff_a( i+1 , alpha , beta)
-		if(i > 0): T[i,i-1] = coeff_a( i , alpha , beta)
+		T[i,i] = coeff_a_matrix(i , alpha , beta)
+		if(i < n): T[i,i+1] = coeff_b_matrix( i+1 , alpha , beta)
+		if(i > 0): T[i,i-1] = coeff_b_matrix( i , alpha , beta)
 	#quadrature points  = eigenvalues
-	#quadrature weights = eigenvector_j[1]^2 * integral w(x), with w(x) = (1-x)^alpha (1+x)^beta. Also wrong defined in lecture notes.
+	#quadrature weights = eigenvector_j[0]^2 * integral w(x), with w(x) = (1-x)^alpha (1+x)^beta.
 	eigenvalue , eigenvector = LA.eig(T)
 	
 	quadrature_points = eigenvalue
 	weight_integral = quad( integrand , -1 , 1 )[0]
-	quadrature_weights = eigenvector[0,:] * weight_integral
+	quadrature_weights = eigenvector[0,:] * eigenvector[0,:] * weight_integral
 	return quadrature_points, quadrature_weights
 
 #d)
-#probably typo in constants
+#Does not find any roots. Doing it like in c) would have been smarter
 def helperfunc(x,alpha,beta,n):
 	return (1-x*x) * jacobiDer(x,alpha,beta,n)
 	
@@ -80,13 +89,15 @@ def JacobiDMat(x,n):
 		if(j > 0):	V[:,j] = jacobiDer(points, 0, 0, j)		#for j = 0 problem!
 				
 		
-alpha=1
-beta=1
+alpha=0
+beta=0
 n=5
 x=np.arange(-1.,1.1,0.1)	
 	
 a,b=JacobiQuad(n,alpha,beta)
 c,d=JacobiLGL(alpha,beta,n)
+
+print(a,b)
 
 JacobiVMat(x,n)
 JacobiDMat(x,n)
